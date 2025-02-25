6d0c4c296eacae283c70a8cefe3422ec2fa123a2
Feben-A <afa508165@gmail.com>
2025-02-25 11:50:19 +0000

    Add nodemon and define scripts in package.json

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  `App is listening on port: ${port}`;
+  console.log(`App is listening on port: ${port}`);
 });
