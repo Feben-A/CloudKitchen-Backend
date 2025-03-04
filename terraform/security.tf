dbe21857ec3761d8660b43cae0ba3d09de1abe55
Feben-A <afa508165@gmail.com>
2025-03-04 14:47:17 +0000

    Add terraform and ansible files

---
+++ terraform/security.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  tags = {
+    name = "http_server_sg"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.http_server_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
