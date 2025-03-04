a823cea77ab71debbdc38544e13b142135fbba31
Sattia12 <sattia1@hotmail.com>
2025-03-04 16:48:36 +0000

    dockerised

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,8 +1,19 @@
-const { Pool } = require("pg");
-
+const { Pool } = require('pg');
+ 
 const db = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 });
-
+ 
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection error', err.stack);
+  } else {
+    console.log('Database connected');
+  }
+});
+ 
 module.exports = db;
